0f0cab4c0086cd1fe8707f6ea861257a864a82e6
sandy <sandeepkumarnavik5@gmail.com>
2023-03-22 23:55:58 +0530

    Added home page and created hero section

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 
-import HelloWorld from '../components/HelloWorld.vue';
+import HomePage from '../components/HomePage.vue';
 import Projects from '../components/Projects.vue';
 import AboutUs from '../components/AboutUs.vue';
 import LoginForm from '../components/LoginForm.vue';
@@ -8,7 +8,7 @@ import LoginForm from '../components/LoginForm.vue';
 
 
 const routes = [
-    { path: '/', component: HelloWorld },
+    { path: '/', component: HomePage },
     { path: '/aboutus',component: AboutUs },
     { path: '/contact',component: Projects },
     { path: '/login',component: LoginForm }
